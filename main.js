a53dfab267132eff2f72ec679a629417c1f27d9c
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-04-26 20:56:36 +0600

    Create hello saga that will called for every INCREMENT action dispatching

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ import "babel-polyfill"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
 import Counter from './Counter'
 import reducer from './reducers'
+import rootSaga from './sagas';
 
-const store = createStore(reducer)
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(rootSaga);
 
-const action = type => store.dispatch({type})
+const action = type => store.dispatch({ type })
 
 function render() {
   ReactDOM.render(
@@ -21,5 +25,6 @@ function render() {
   )
 }
 
-render()
+render();
+
 store.subscribe(render)
